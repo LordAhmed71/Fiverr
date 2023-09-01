dadf25a97b6f8584dc5cf3dd477ae8a8c9e75dfb
LordAhmed71 <ahmed55mohamed88@gmail.com>
2023-09-02 00:28:49 +0300

    final front end

---
+++ client/src/components/slide/Slide.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Slide.scss";
+import Slider from "infinite-react-carousel";
+
+
+const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+  return (
+    <div className="slide">
+      <div className="container">
+         <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll} autoplay={true} autoplayScroll={1} autoplaySpeed	={5000} duration={3000} arrows={false} shift={5}>
+          {children}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
